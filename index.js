53663e6da9e81669d63d6eda7dfdd60cdc28ac7d
Prashanth Moorthy <chapparfellow@gmail.com>
2018-11-08 23:47:26 +0530

    No change triggering on Heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 var WebSocketServer = require('websocket').server;
 var http = require('http');
- 
+  
 var server = http.createServer(function(request, response) {
     console.log((new Date()) + ' Received request for ' + request.url);
     response.writeHead(404);
